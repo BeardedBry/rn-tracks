a1b67cc2a2ede8c1914d3eee62ee3daf90dd3bba
Burkard <burkarbr@LAPTOP-BURKARBR.local>
2020-04-13 15:31:05 -0400

    AuthContext 	- added signup action 	- used async storage to store created JWT
    App.js
    	- imported navigationRef and passed it our navigation object.
    
    navigationRef
    	- created a reference or hook for a context and other components to have access to navigation

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,30 +1,36 @@
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import AsyncStorage from '@react-native-community/async-storage';
+import {navigate} from '../navigationRef';
 
 const authReducer = (state, action) => {
     switch (action.type){
         case 'add_error':
-            return {...state, errorMessage: action.payload}
-            break;
+            return {...state, errorMessage: action.payload};
+        case 'signup':
+            return {errorMessage: '', token: action.payload};
         default:
             return state;
     }
 };
 
-const signup = (dispatch) => {
-    return async ({email, password}) => {
-        // make api request to sign up with email and password.
-        // if we sign up, modify state, and say we are authenticated.
-        // if signup fails, send error.
-        try {
-            const response = await trackerApi.post('/signup', {email, password});
-            console.log(response.data);
-        }catch (err) {
-            //console.log(err.message, err.response.data);
-            dispatch({type: 'add_error', payload: 'Something went wrong with sign up.'})
-        }
-    };
+const signup = (dispatch) => async ({email, password}) => {
+    // make api request to sign up with email and password.
+    // if we sign up, modify state, and say we are authenticated.
+    // if signup fails, send error.
+    try {
+        const response = await trackerApi.post('/signup', {email, password});
+        // returns a JWT, Json Web Token
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({type: 'signup', payload: response.data.token});
+        // navigate to main flow
+        navigate('TrackList');
+    }catch (err) {
+        //console.log(err.message, err.response.data);
+        dispatch({type: 'add_error', payload: 'Something went wrong with sign up.'})
+    }
 };
+
 
 const signin = (dispatch) => {
     return ({email, password}) => {
@@ -43,5 +49,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     {signin, signout, signup}, // functions we will make
-    { isSignedIn: false, errorMessage: ''} // initial state, (placeholder isSignedIn: false)
+    { token: null, errorMessage: ''} // initial state, (placeholder isSignedIn: false)
 )
